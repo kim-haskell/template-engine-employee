9a63de2ada03ba5ba9ed785e74e1798ad22201c0
Kim Haskell <kim.haskell03@gmail.com>
2020-07-27 16:58:26 -0600

    updating app.js file

---
+++ app.js
@@ -0,0 +1,173 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const teamMembers = [];
+const idArray = [];
+
+function mainMenu () {
+    function createManager () {
+        console.log("Start building your team")
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the manager's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter a valid input";
+                }
+
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the manager's id?",
+                // put in a validate here
+            },
+            {
+                type: "input", 
+                name: "managerEmail",
+                message: "What is your manager's email?",
+                // validate
+            },
+            {
+                type: "input", 
+                name: "managerOfficeNumber",
+                message: "What is your manager's office number?",
+                //validate
+            }
+        ]).then(res => {
+            const manager = new Manager(res.managerName, res.managerId, res.managerEmail, res.managerOfficeNumber)
+            teamMembers.push(manager);
+            idArray.push(res.managerId);
+            createTeam();
+        })
+    }
+    function createTeam () {
+        inquirer.prompt ([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "Which type of employee would you like to add?",
+                choices: [
+                    "engineer", "intern", "no more team members"
+                ]
+            }
+        ]).then(userRes => {
+            switch(userRes.memberChoice) {
+                case "engineer": 
+                addEngineer();
+                break
+                case "intern":
+                addIntern();
+                break
+                default:
+                buildTeam();     
+            }
+        })
+
+    }
+    function addEngineer () {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the engineer's name?"
+            },
+            {
+                type: "input", 
+                name: "engineerId",
+                message: "What is the engineer's id?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the engineer's email?"
+            },
+            {
+                type: "input",
+                name: "engineerGitHub",
+                message: "What is the engineer's github?"
+            }
+
+        ]).then(res => {
+            const engineer = new Engineer(res.engineerName, res.engineerId, res.engineerEmail, res.engineerGitHub)
+            teamMembers.push(engineer);
+            idArray.push(res.engineerId);
+            createTeam();
+        }) 
+    }
+    function addIntern () {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is the intern's name?"
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "What is the intern's id?"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is the intern's email?"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What is the intern's school?"
+            }
+
+        ]).then(res => {
+            const intern = new Intern (res.internName, res.internId, res.internEmail, res.internSchool);
+            teamMembers.push(intern);
+            idArray.push(res.internId);
+            createTeam();
+        })
+    }
+    function buildTeam () {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamMembers), "utf-8")
+    }
+    createManager();
+
+}
+
+    mainMenu();
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
